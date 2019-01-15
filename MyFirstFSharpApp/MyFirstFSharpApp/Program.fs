10e108fc29a22b3fc1660e8fb9da2d8cb0b115d2
Florian Eiden <florian.eiden@fleid.fr>
2019-01-15 14:29:42 -0800

    Change the ouput to the program parameter value

--- MyFirstFSharpApp/MyFirstFSharpApp/Program.fs
+++ MyFirstFSharpApp/MyFirstFSharpApp/Program.fs
@@ -2,7 +2,8 @@
 
 open System
 
-[<EntryPoint>]
+[<EntryPoint>] // Attribute telling F# that this is the starting point
 let main argv =
-    printfn "Hello World from F#!"
+    //printfn "Hello World from F#!"
+    printfn "%A" argv // will return [||], empty array, if no value in input
     0 // return an integer exit code
