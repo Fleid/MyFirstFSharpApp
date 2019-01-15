6576f419fd8bfeb691e3ca0fe2ee207b8ec62940
Florian Eiden <florian.eiden@fleid.fr>
2019-01-15 15:26:04 -0800

    Add sample function to the script

--- MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
@@ -1,3 +1,12 @@
-﻿// Right click : execute in interactive
+﻿// alt enter : execute in interactive
 
 let text = "Hello, world"
+printfn "This is your value: %A" text
+text.Length
+
+
+let greetPerson name age =
+    sprintf "Hello, %s. You are %d years old" name age
+
+let greeting = greetPerson "Fred" 25
+
