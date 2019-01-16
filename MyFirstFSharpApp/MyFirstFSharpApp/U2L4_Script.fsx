6d7ade3b0b48d4a56bb07677db085c4a086f2193
Florian Eiden <florian.eiden@fleid.fr>
2019-01-16 13:40:58 -0800

    Text basic function binding

--- MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
@@ -6,3 +6,7 @@ let randomGenerator = System.Random()
 let randomString = randomGenerator.Next(10).ToString()
 printfn "My random number = %A" randomString
 
+let getNext () =
+    randomGenerator.Next(10)
+
+getNext()
