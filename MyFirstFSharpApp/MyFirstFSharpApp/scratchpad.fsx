31041377689e230353748d7eb7aabaf606ebeda8
Florian Eiden <florian.eiden@fleid.fr>
2019-01-15 16:08:48 -0800

    Solve try this for Chapter 2

--- MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
@@ -1,4 +1,7 @@
-﻿// alt enter : execute in interactive
+﻿open System.IO
+open System
+
+// alt enter : execute in interactive
 
 let text = "Hello, world"
 printfn "This is your value: %A" text
@@ -10,3 +13,15 @@ let greetPerson name age =
 
 let greeting = greetPerson "Fred" 25
 
+let countWords (text:string) =
+    text.Split(' ').Length
+
+
+let myText = "This is my perfect example"
+let countedWords = countWords myText
+let outputArray = [myText; countedWords.ToString()]
+let outputText = String.concat ", " outputArray
+
+let outputFileName = @"c:\temp\tryThisChapter2.txt"
+
+File.WriteAllText(outputFileName, outputText)
