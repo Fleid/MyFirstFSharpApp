58e902bae6a0071a339c78bb216bea8f89751b31
Florian Eiden <florian.eiden@fleid.fr>
2019-01-16 13:55:11 -0800

    Test nested functions

--- MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
@@ -32,3 +32,19 @@ let estimagedAge =
         let year = DateTime.Now.Year
         year - 1980
     sprintf "You are about %d years old!" age
+
+// 4.7
+
+let estimateAges (familyName, year1, year2, year3) =
+    let calculateAge yearOfBirth =
+        let year = System.DateTime.Now.Year
+        year - yearOfBirth
+    
+    let estimatedAge1 = calculateAge year1
+    let estimatedAge2 = calculateAge year2
+    let estimatedAge3 = calculateAge year3
+
+    let averageAge = (estimatedAge1 + estimatedAge2 + estimatedAge3) / 3
+    sprintf "Average age for family %s is %d" familyName averageAge
+
+estimateAges("E",1980,1990,2018)
