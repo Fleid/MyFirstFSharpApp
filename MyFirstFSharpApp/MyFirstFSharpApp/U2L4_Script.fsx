7a926b04190e497e59efca740dc3855c36018bac
Florian Eiden <florian.eiden@fleid.fr>
2019-01-16 13:38:02 -0800

    Add basic bindings

--- MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/U2L4_Script.fsx
@@ -1,1 +1,8 @@
 ﻿
+let name = "Florian"
+let age = 38
+
+let randomGenerator = System.Random()
+let randomString = randomGenerator.Next(10).ToString()
+printfn "My random number = %A" randomString
+
