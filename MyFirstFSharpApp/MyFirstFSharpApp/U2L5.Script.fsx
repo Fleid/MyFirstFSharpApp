5cb5e139621fac70b349866fe9c2eb93d48b07d1
Florian Eiden <florian.eiden@fleid.fr>
2019-01-16 14:22:06 -0800

    Expore type inference

--- MyFirstFSharpApp/MyFirstFSharpApp/U2L5.Script.fsx
+++ MyFirstFSharpApp/MyFirstFSharpApp/U2L5.Script.fsx
@@ -1,9 +1,21 @@
-﻿
+﻿open System.Net.Mime.MediaTypeNames
+
 // 5.5
 
-let add (a, b) = 
-    a + b
-
-let add2 (a:int, b:int) : int = 
+let add (a:int, b:int) : int = 
     let answer:int = a + b
     answer
+
+    
+let add2 (a, b) = 
+    a + b //default binding of + is int
+
+let add3 (a:float, b) =
+    a + b //no implicit conversion allowed : constraining the type via parameter
+
+let add4 (a, b) =
+    a + b + "Hello"
+
+let add5 (a, b) =
+    (a + b) * 1.0
+
